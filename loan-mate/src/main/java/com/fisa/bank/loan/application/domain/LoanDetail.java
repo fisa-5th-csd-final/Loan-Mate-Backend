98aa8a1485d49f92c95d1e919c33c90589f9e1fd
cheongjay <wjdjoonim@gmail.com>
2025-11-14 18:20:53 +0900

    feat: core banking 서버에 대출 상세 정보 요청 기능

---
+++ loan-mate/src/main/java/com/fisa/bank/loan/application/domain/LoanDetail.java
@@ -0,0 +1,18 @@
+package com.fisa.bank.loan.application.domain;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+import com.fisa.bank.loan.persistence.enums.LoanType;
+import com.fisa.bank.loan.persistence.enums.RepaymentType;
+
+@Getter
+public class LoanDetail {
+  private BigDecimal remainPrincipal;
+  private BigDecimal principal;
+  private BigDecimal monthlyRepayment;
+  private String accountNumber;
+  private LoanType loanType;
+  private RepaymentType repaymentType;
+}
