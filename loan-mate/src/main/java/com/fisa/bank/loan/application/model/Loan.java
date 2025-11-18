5e65c42b46f82bb354be144547e6f62d8c05aac9
cheongjay <wjdjoonim@gmail.com>
2025-11-18 11:16:39 +0900

    fix: 납부 개월 수 로직 수정, N+1문제 수정

--- loan-mate/src/main/java/com/fisa/bank/loan/application/model/Loan.java
+++ loan-mate/src/main/java/com/fisa/bank/loan/application/model/Loan.java
@@ -3,13 +3,20 @@ package com.fisa.bank.loan.application.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 import com.fisa.bank.loan.persistence.enums.RiskLevel;
+import com.fisa.bank.persistence.loan.enums.RepaymentStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class Loan {
   private final Long loanId;
   private final String loanName;
+  private final LocalDateTime createdAt;
+  private final LocalDateTime lastRepaymentDate;
+  private final int term;
+  private final RepaymentStatus repaymentStatus;
   private RiskLevel riskLevel;
   private LoanDetail loanDetail;
 }
