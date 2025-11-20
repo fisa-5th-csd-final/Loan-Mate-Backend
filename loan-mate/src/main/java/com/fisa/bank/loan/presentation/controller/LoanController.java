7019cbe97c54e65973402fab582affa47c6300ee
hyebim <shb010424@gmail.com>
2025-11-20 15:28:22 +0900

    refactor: 기존 코드랑 스타일 맞춤

--- loan-mate/src/main/java/com/fisa/bank/loan/presentation/controller/LoanController.java
+++ loan-mate/src/main/java/com/fisa/bank/loan/presentation/controller/LoanController.java
@@ -40,11 +40,11 @@ public class LoanController {
     return ApiResponseGenerator.success(ResponseCode.GET, loanDetail);
   }
 
-  @GetMapping("/{loanLedgerId}/auto-deposit")
+  @GetMapping("/ledgers/{loanId}/auto-deposit")
   public ApiResponse<SuccessBody<LoanAutoDepositResponse>> getAutoDeposit(
-      @PathVariable Long loanLedgerId) {
-    log.info("자동 예치 여부 조회");
-    LoanAutoDepositResponse response = manageLoanUseCase.getAutoDeposit(loanLedgerId);
+      @PathVariable Long loanId) {
+    log.info("자동 예치 여부 조회. loanId={}", loanId);
+    LoanAutoDepositResponse response = manageLoanUseCase.getAutoDeposit(loanId);
 
     return ApiResponseGenerator.success(ResponseCode.GET, response);
   }
