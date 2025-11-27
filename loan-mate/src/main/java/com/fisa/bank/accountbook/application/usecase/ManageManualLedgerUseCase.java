2805e1d54e7580275eb06735f87c34c1973aa902
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-27 23:51:15 +0900

    feat: manual ledger 도메인 usecase 추가(조회, 추가)

---
+++ loan-mate/src/main/java/com/fisa/bank/accountbook/application/usecase/ManageManualLedgerUseCase.java
@@ -0,0 +1,14 @@
+package com.fisa.bank.accountbook.application.usecase;
+
+import java.util.List;
+
+import com.fisa.bank.accountbook.application.dto.request.ManualLedgerCreateRequest;
+import com.fisa.bank.accountbook.application.dto.response.ManualLedgerResponse;
+import com.fisa.bank.accountbook.application.model.ManualLedgerType;
+
+public interface ManageManualLedgerUseCase {
+
+  ManualLedgerResponse addEntry(ManualLedgerCreateRequest request);
+
+  List<ManualLedgerResponse> getEntries(ManualLedgerType type);
+}
