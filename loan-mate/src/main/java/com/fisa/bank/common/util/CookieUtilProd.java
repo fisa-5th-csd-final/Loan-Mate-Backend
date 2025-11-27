e8dd869a761f4318280b90d0a582285fdb8927e0
Bellmin <whdals011203@naver.com>
2025-11-27 17:54:10 +0900

    [CHORE] 쿠키 설정 추가 (#52)

--- loan-mate/src/main/java/com/fisa/bank/common/util/CookieUtilProd.java
+++ loan-mate/src/main/java/com/fisa/bank/common/util/CookieUtilProd.java
@@ -1,5 +1,6 @@
 package com.fisa.bank.common.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,13 @@ import org.springframework.stereotype.Component;
 @Profile("prod")
 public class CookieUtilProd implements CookieUtil {
 
+  @Value("${jwt.cookie.domain}")
+  private String cookieDomain;
+
   @Override
   public ResponseCookie createHttpOnlyCookie(String name, String value, int maxAgeSeconds) {
     return ResponseCookie.from(name, value)
+        .domain(cookieDomain)
         .httpOnly(true)
         .secure(true) // prod는 https만 사용
         .sameSite("None") // 크로스 도메인 필수
@@ -22,6 +27,7 @@ public class CookieUtilProd implements CookieUtil {
   @Override
   public ResponseCookie deleteCookie(String name) {
     return ResponseCookie.from(name, "")
+        .domain(cookieDomain)
         .httpOnly(true)
         .secure(true)
         .sameSite("None")
