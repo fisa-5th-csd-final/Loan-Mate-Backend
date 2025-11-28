15eabc2dd544b268d7b1a7d8fa7f8ac4674c3a54
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-28 21:09:58 +0900

    del: 필요 없어진 business exception 예외 처리 삭제

--- loan-mate/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
+++ loan-mate/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.http.HttpStatus;
 
 import com.fisa.bank.accountbook.application.exception.ManualLedgerAccessDeniedException;
-import com.fisa.bank.accountbook.application.exception.ManualLedgerInvalidRequestException;
 import com.fisa.bank.accountbook.application.exception.ManualLedgerNotFoundException;
 import com.fisa.bank.common.application.exception.AlreadyDeletedException;
 import com.fisa.bank.common.application.exception.BusinessException;
@@ -20,7 +19,6 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
   /** 여기에 커스텀 BusinessException을 정의하면 됩니다. */
   // Common
   ALREADY_DELETED_EXCEPTION(HttpStatus.NOT_FOUND, AlreadyDeletedException.class),
-  MANUAL_LEDGER_INVALID_REQUEST(HttpStatus.BAD_REQUEST, ManualLedgerInvalidRequestException.class),
   MANUAL_LEDGER_NOT_FOUND(HttpStatus.NOT_FOUND, ManualLedgerNotFoundException.class),
   MANUAL_LEDGER_ACCESS_DENIED(HttpStatus.FORBIDDEN, ManualLedgerAccessDeniedException.class);
 
