7a8c6ee753d5d8ce256b7ff59df2156847cd011c
Bellmin <whdals011203@naver.com>
2025-11-28 16:11:24 +0900

    refactor: LoanComment JsonIgnore 추가

--- loan-mate/src/main/java/com/fisa/bank/loan/application/model/LoanComment.java
+++ loan-mate/src/main/java/com/fisa/bank/loan/application/model/LoanComment.java
@@ -1,5 +1,6 @@
 package com.fisa.bank.loan.application.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Getter;
 @Builder
 @Getter
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoanComment {
   private final Long loanLedgerId;
   private final String comment;
