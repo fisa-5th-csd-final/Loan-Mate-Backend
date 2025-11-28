837ba694d1ff883e8d3bf74d10360e0834fe795f
Bellmin <whdals011203@naver.com>
2025-11-28 16:14:03 +0900

    refactor: LoanComment 기본 생성자 추가

--- loan-mate/src/main/java/com/fisa/bank/loan/application/model/LoanComment.java
+++ loan-mate/src/main/java/com/fisa/bank/loan/application/model/LoanComment.java
@@ -2,14 +2,16 @@ package com.fisa.bank.loan.application.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LoanComment {
-  private final Long loanLedgerId;
-  private final String comment;
+  private Long loanLedgerId;
+  private String comment;
 }
