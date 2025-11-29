325e12f41641dd636dcecec0e6995bd7bcdb3797
ehgkals <ehgkals12@gmail.com>
2025-11-29 12:26:04 +0900

    [FIX]ì»

--- loan-mate/src/main/java/com/fisa/bank/account/presentation/controller/AccountController.java
+++ loan-mate/src/main/java/com/fisa/bank/account/presentation/controller/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
