a66a5903d853160d77118d420115b8e9dbf01ba5
Park Jun Sang <145450976+ehgkals@users.noreply.github.com>
2025-11-29 21:34:01 +0900

    Revert "[FEAT] 계좌 정보 조회 API 구현 (#78)"
    This reverts commit 98ab630e93e968e43f799c608ce72376c3b4ec9a.

--- loan-mate/src/main/java/com/fisa/bank/account/persistence/repository/JpaAccountRepository.java
+++ loan-mate/src/main/java/com/fisa/bank/account/persistence/repository/JpaAccountRepository.java
@@ -1,19 +1,11 @@
 package com.fisa.bank.account.persistence.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 import com.fisa.bank.persistence.account.entity.Account;
-import com.fisa.bank.persistence.account.entity.id.AccountId;
-import com.fisa.bank.persistence.user.entity.id.UserId;
+import com.fisa.bank.persistence.account.repository.AccountRepository;
+import com.fisa.bank.persistence.user.entity.User;
 
-public interface JpaAccountRepository extends JpaRepository<Account, AccountId> {
-  @Query("""
-        select a from Account a
-        where a.user.userId = :userId
-    """)
-  List<Account> findAllByUserId(@Param("userId") UserId userId);
+public interface JpaAccountRepository extends AccountRepository {
+  Optional<Account> findFirstByUserAndIsForIncomeTrue(User user);
 }
