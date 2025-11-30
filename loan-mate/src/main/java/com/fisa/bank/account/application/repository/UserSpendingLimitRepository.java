d9dbbc7a900a50416d851278f138596cbd6b2e1b
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-30 13:52:10 +0900

    feat: 사용자 설정 지출 한도 조회하는 application 계층 Repository

---
+++ loan-mate/src/main/java/com/fisa/bank/account/application/repository/UserSpendingLimitRepository.java
@@ -0,0 +1,12 @@
+package com.fisa.bank.account.application.repository;
+
+import java.util.Optional;
+
+import com.fisa.bank.account.application.model.UserSpendingLimit;
+
+public interface UserSpendingLimitRepository {
+
+  UserSpendingLimit save(UserSpendingLimit limit);
+
+  Optional<UserSpendingLimit> findByServiceUserId(Long serviceUserId);
+}
