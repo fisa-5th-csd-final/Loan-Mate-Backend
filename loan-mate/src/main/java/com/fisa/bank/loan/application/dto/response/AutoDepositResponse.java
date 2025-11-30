cc95f9c873851045f86708e982231a38f8d7597e
ehgkals <ehgkals12@gmail.com>
2025-12-01 00:10:32 +0900

    [FIX]응답값 수정

--- loan-mate/src/main/java/com/fisa/bank/loan/application/dto/response/AutoDepositResponse.java
+++ loan-mate/src/main/java/com/fisa/bank/loan/application/dto/response/AutoDepositResponse.java
@@ -1,5 +1,6 @@
 package com.fisa.bank.loan.application.dto.response;
 
+import com.fisa.bank.persistence.loan.entity.id.LoanLedgerId;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 @Getter
 @Builder
 public class AutoDepositResponse {
+  private LoanLedgerId loanLedgerId;
   private String loanName;
   private BigDecimal accountBalance;
   private boolean autoDepositEnabled;
