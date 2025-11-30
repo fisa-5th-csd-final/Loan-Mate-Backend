d9e0fd6d21292eccd6eddc7f88221b692422ab1c
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-30 13:53:40 +0900

    feat: application repository 구현체

---
+++ loan-mate/src/main/java/com/fisa/bank/account/persistence/repository/UserSpendingLimitRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.fisa.bank.account.persistence.repository;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.fisa.bank.account.application.model.UserSpendingLimit;
+import com.fisa.bank.account.application.repository.UserSpendingLimitRepository;
+import com.fisa.bank.account.persistence.UserSpendingLimitMapper;
+import com.fisa.bank.account.persistence.entity.UserSpendingLimitEntity;
+
+@Repository
+@RequiredArgsConstructor
+public class UserSpendingLimitRepositoryImpl implements UserSpendingLimitRepository {
+
+  private final JpaUserSpendingLimitRepository jpaRepository;
+  private final UserSpendingLimitMapper mapper;
+
+  @Override
+  public UserSpendingLimit save(UserSpendingLimit limit) {
+    UserSpendingLimitEntity saved = jpaRepository.save(mapper.toEntity(limit));
+    return mapper.toDomain(saved);
+  }
+
+  @Override
+  public Optional<UserSpendingLimit> findByServiceUserId(Long serviceUserId) {
+    return jpaRepository.findByServiceUserId(serviceUserId).map(mapper::toDomain);
+  }
+}
